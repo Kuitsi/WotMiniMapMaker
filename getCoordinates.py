e409d97c8522a7338b918fb52961fe8d379ca323
Juha Kuitunen <kuitsi@iki.fi>
2013-01-04 19:28:13 +0200

    added coordinate conversion to getCoordinates.py

--- getCoordinates.py
+++ getCoordinates.py
@@ -23,15 +23,17 @@ def readxml(filename):
 
     bottomLeft = soup.find("bottomLeft")
     upperRight = soup.find("upperRight")
-    bottomLeft = bottomLeft.contents
-    upperRight = upperRight.contents
-    x2, y2 = string.split(bottomLeft[0])
-    x1, y1 = string.split(upperRight[0])
-    x2, y2 = float(x2), float(y2)
-    x1, y1 = float(x1), float(y1)
+    bottomLeft = bottomLeft.contents[0]
+    upperRight = upperRight.contents[0]
 
-    print "bottomLeft:\t" + str(x2) + "\t" + str(y2)
-    print "upperRight:\t" + str(x1) + "\t" + str(y1)
+    # Bounding box: top left (x1,y1), bottom right (x2,y2)
+    x1, y2 = string.split(bottomLeft)
+    x2, y1 = string.split(upperRight)
+    x1, y2 = float(x1), float(y2)
+    x2, y1 = float(x2), float(y1)
+
+    print "bottomLeft:\t" + str(x1) + "\t" + str(y2)
+    print "upperRight:\t" + str(x2) + "\t" + str(y1)
 
     ctf = soup.find("ctf")
     ass = soup.find("assault")
@@ -46,9 +48,9 @@ def readxml(filename):
 
         print "team positions:"
         x, y = getCoords(team1Position.contents[0]);
-        print "team 1:\t" + str(x) + "\t" + str(y)
+        print "team 1:\t" + str(x) + "\t" + str(y) + "\t" + "converted:\t" + str(getConvertedXY(team1Position.contents[0], x1, y1, x2, y2))
         x, y = getCoords(team2Position.contents[0])
-        print "team 2:\t" + str(x) + "\t" + str(y)
+        print "team 2:\t" + str(x) + "\t" + str(y) + "\t" + "converted:\t" + str(getConvertedXY(team2Position.contents[0], x1, y1, x2, y2))
 
         teamSpawnPoints = ctf.find("teamSpawnPoints")
         if teamSpawnPoints != None:
@@ -60,16 +62,16 @@ def readxml(filename):
             print "spawn points:"
             for spawn in team1SpawnPoint:
                 x, y = getCoords(spawn.contents[0]);
-                print "team 1:\t" + str(x) + "\t" + str(y)
+                print "team 1:\t" + str(x) + "\t" + str(y) + "\t" + "converted:\t" + str(getConvertedXY(spawn.contents[0], x1, y1, x2, y2))
             for spawn in team2SpawnPoint:
                 x, y = getCoords(spawn.contents[0]);
-                print "team 2:\t" + str(x) + "\t" + str(y)
+                print "team 2:\t" + str(x) + "\t" + str(y) + "\t" + "converted:\t" + str(getConvertedXY(spawn.contents[0], x1, y1, x2, y2))
 
     if ass != None:
         print "ASS"
         BasePosition = ass.find("teamBasePositions").find("team1").find(re.compile("^position"))
         x, y = getCoords(BasePosition.contents[0]);
-        print "base:\t" + str(x) + "\t" + str(y)
+        print "base:\t" + str(x) + "\t" + str(y) + "\t" + "converted:\t" + str(getConvertedXY(BasePosition.contents[0], x1, y1, x2, y2))
 
         teamSpawnPoints = ass.find("teamSpawnPoints")
         if teamSpawnPoints != None:
@@ -81,16 +83,16 @@ def readxml(filename):
             print "spawn points:"
             for spawn in team1SpawnPoint:
                 x, y = getCoords(spawn.contents[0]);
-                print "team 1:\t" + str(x) + "\t" + str(y)
+                print "team 1:\t" + str(x) + "\t" + str(y) + "\t" + "converted:\t" + str(getConvertedXY(spawn.contents[0], x1, y1, x2, y2))
             for spawn in team2SpawnPoint:
                 x, y = getCoords(spawn.contents[0]);
-                print "team 2:\t" + str(x) + "\t" + str(y)
+                print "team 2:\t" + str(x) + "\t" + str(y) + "\t" + "converted:\t" + str(getConvertedXY(spawn.contents[0], x1, y1, x2, y2))
 
     if dom != None:
         print "DOM"
         controlPoint = dom.find("controlPoint")
         x, y = getCoords(controlPoint.contents[0]);
-        print "base:\t" + str(x) + "\t" + str(y)
+        print "base:\t" + str(x) + "\t" + str(y) + "\t" + "converted:\t" + str(getConvertedXY(controlPoint.contents[0], x1, y1, x2, y2))
         teamSpawnPoints = dom.find("teamSpawnPoints")
         team1 = teamSpawnPoints.find("team1")
         team2 = teamSpawnPoints.find("team2")
@@ -100,10 +102,10 @@ def readxml(filename):
         print "spawn points:"
         for spawn in team1SpawnPoint:
             x, y = getCoords(spawn.contents[0]);
-            print "team 1:\t" + str(x) + "\t" + str(y)
+            print "team 1:\t" + str(x) + "\t" + str(y) + "\t" + "converted:\t" + str(getConvertedXY(spawn.contents[0], x1, y1, x2, y2))
         for spawn in team2SpawnPoint:
             x, y = getCoords(spawn.contents[0]);
-            print "team 2:\t" + str(x) + "\t" + str(y)
+            print "team 2:\t" + str(x) + "\t" + str(y) + "\t" + "converted:\t" + str(getConvertedXY(spawn.contents[0], x1, y1, x2, y2))
 
     print "\n"
 
@@ -113,6 +115,31 @@ def getCoords(input):
     x, y = float(inx.replace(',', '.')), float(iny.replace(',', '.'))
     return x, y
 
+
+def getConvertedXY(input, x1, y1, x2, y2):
+    inx, iny = string.split(input)
+    inx, iny = float(inx.replace(',', '.')), float(iny.replace(',', '.'))
+
+    maxX = x2 - min(x1,x2) # X and Y axis should be equals in length
+
+    # move coordinates to start from (0,0)
+    inx = inx - min(x1,x2)
+    iny = iny - min(y1,y2)
+
+    inx, iny = scaleCoordinates(inx, iny, maxX, 500.0)
+    inx, iny = int(round(inx)), int(round(iny))
+
+    # set icon center at coordinates (icons 64x64)
+    #inx -= 24
+    #iny += 24
+
+    return inx, iny
+
+
+def scaleCoordinates(x, y, oldMax, newMax):
+    x = x / oldMax * newMax
+    y = y / oldMax * newMax
+    return x, y
 
 if __name__ == '__main__':
     main()
