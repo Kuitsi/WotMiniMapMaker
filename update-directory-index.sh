dc18650f10a38bc3aad6b5f3bed756fa31618c50
Juha Kuitunen <kuitsi@iki.fi>
2014-01-12 14:50:25 +0200

    modified directory index creating script to do only what is required, nothing more

--- update-directory-index.sh
+++ update-directory-index.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-for DIR in $(find ./maps -type d); do
+if [ $# -lt 1 ]; then
+    echo "ERROR: $0 requires WoT version number as a first parameter"
+elif [ $# -ge 1 ]; then
   (
-    echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
-    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "index.html" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }' 
+    echo -e "<html>\n<body>\n<h1>World of Tanks (version "$1") minimaps</h1>\n"
+    echo -e "<p>Maps generated using <a href="https://github.com/Kuitsi/WotMiniMapMaker">WotMiniMapMaker</a>.<br />"
+    echo -e "Team 1 = green, Team 2 = red.</p>\n<hr/>\n<pre>"
+    ls -1pa ./maps | grep -v "^\./$" | grep -v "^\.\./$" | awk '{ printf "<a href=\"maps/%s\">%s</a>\n",$1,$1 }' 
     echo -e "</pre>\n</body>\n</html>"
-  ) > "${DIR}/index.html"
-done
+  ) > index.html
+fi
